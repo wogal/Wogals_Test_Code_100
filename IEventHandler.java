e3d9fb1410483ec8cfadabbbdd95e58b220165ce
wogal <egilgeous@gmail.com>
2017-04-17 16:07:15 -0400

    // events and call backs from vid -- https://www.youtube.com/watch?v=QnD32SFgGXY --

---
+++ IEventHandler.java
@@ -0,0 +1,9 @@
+package Call_Back_Test_Code_Events;
+
+/**
+ * Created by wogal on 4/17/2017.
+ */
+
+public interface IEventHandler {
+    public void Callback (Event _event);
+}
---
+++ Listener.java
@@ -0,0 +1,8 @@
+package Call_Back_Test_Code_Events;
+
+/**
+ * Created by wogal on 4/17/2017.
+ */
+
+public class Listener {
+}
