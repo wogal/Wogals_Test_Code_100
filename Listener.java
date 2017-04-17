a464cd5be4ca0d9df97e0f9db528292402e13c8b
wogal <egilgeous@gmail.com>
2017-04-17 16:21:06 -0400

    // events and call backs from vid -- https://www.youtube.com/watch?v=QnD32SFgGXY --

--- Listener.java
+++ Listener.java
@@ -5,4 +5,21 @@ package Call_Back_Test_Code_Events;
  */
 
 public class Listener {
+    private String type;
+    private IEventHandler handler;
+
+    public Listener (String _type, IEventHandler _handler) {
+        this.type = _type;
+        this.handler = _handler;
+    }
+
+    public String getType () {
+        return this.type;
+    }
+
+    public IEventHandler getHandler () {
+        return this.handler;
+    }
+
+
 }
